0db43b9b90ff13c0db9116e691bbdfd5f00e2f26
SamueleGigante <79839432+GigSam@users.noreply.github.com>
2022-04-28 14:12:18 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -15,6 +15,11 @@ def pil_loader(path):
 
 
 class Caltech(VisionDataset):
+    
+    trainCategories = []
+    testCategories = []
+    labels = []
+    
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
@@ -30,6 +35,39 @@ class Caltech(VisionDataset):
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
+        
+        i=0
+        
+        if split == 'train':
+            with open('train.txt', 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.split("/")[0]
+                    if !self.categories.contains(line) && line != 'BACKGROUND_Google':
+                        self.categories.append(line)
+                        i++
+                        self.labels.append(i)
+                    
+                    
+        
+        i=0
+        
+        if split == 'test':
+            with open('test.txt', 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.split("/")[0]
+                    if !self.categories.contains(line) && line != 'BACKGROUND_Google':
+                        self.categories.append(line)
+                        i++
+                        self.labels.append(i)
+            
+        if set(self.trainCategories) != set(self.testCategories):
+            print("Error: train and test sets contain different categories!\n")
+
+            
+        
+        
     def __getitem__(self, index):
         '''
         __getitem__ should access an element through its index
@@ -40,7 +78,7 @@ class Caltech(VisionDataset):
             tuple: (sample, target) where target is class_index of the target class.
         '''
 
-        image, label = ... # Provide a way to access image and label via index
+        image, label = , self.labels[index] # Provide a way to access image and label via index
                            # Image should be a PIL Image
                            # label can be int
 
@@ -55,5 +93,5 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+        length = self.labels.size() # Provide a way to get the length (number of elements) of the dataset
         return length
